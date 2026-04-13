f71f7a05944d8288397a793701f3d08ce414b3c3
TrookeM <158601984+TrookeM@users.noreply.github.com>
2026-04-13 08:46:43 +0200

    Added satellite object

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 function App() {
+
+  const [satellites, setSatellites] = useState([
+    { id: 1, name: 'Galileo-1', status: 'Online', battery: 85 },
+    { id: 2, name: 'Kepler-X', status: 'Maintenance', battery: 40 },
+    { id: 3, name: 'Kepler-X', status: 'Maintenance', battery: 16 },
+  ]);
+
   return (
-    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
-      <h1>🛰️ DLR Satellite Control</h1>
-      <hr />
-      <satellite-card name="Galileo-1" status="Online" battery="85"></satellite-card>
-    </div>
+    <kor-page>
+      {/* slot="top" ancla la barra arriba. label le da el título. */}
+      <kor-app-bar slot="top" label="🛰️ DLR Satellite Control"></kor-app-bar>
+
+      {/* Todo lo que no tenga slot, va al centro de la página automáticamente */}
+      <kor-grid>
+        {satellites.map((sat) => (
+          <satellite-card
+            key={sat.id}
+            name={sat.name}
+            status={sat.status}
+            battery={sat.battery}
+          ></satellite-card>
+        ))}
+      </kor-grid>
+    </kor-page>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
+import { useState } from 'react';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './components/SatelliteCard.js';
 import App from './App.jsx'
+import '@kor-ui/kor';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
